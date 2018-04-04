49793905177d633e570d3133ab22340956972143
Ahed <ahed.ist@gmail.com>
2018-04-04 20:33:06 +0300

    matalan

--- app/src/main/java/com/example/jorjborj/ordrs/MainActivity.java
+++ app/src/main/java/com/example/jorjborj/ordrs/MainActivity.java
@@ -27,7 +27,7 @@ import java.util.ArrayList;
  */
 
  public class MainActivity extends AppCompatActivity {
-
+ // matalan
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
