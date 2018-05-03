84eb64f86387606d74f1deb9d6436632a7859a0a
jorjborj <GEORGES.OUN@GMAIL.COM>
2018-05-03 17:47:12 +0300

    Upcoming Events is Ready to grab data

--- app/src/main/java/com/example/jorjborj/ordrs/UpcomingEventsActivity.java
+++ app/src/main/java/com/example/jorjborj/ordrs/UpcomingEventsActivity.java
@@ -21,21 +21,44 @@ public class UpcomingEventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcoming_events);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         ListView lv = (ListView) findViewById(R.id.eventslv);
 
         events = new ArrayList<>();
         initializeDummyEvents();
 
-        lv.setAdapter(new EventsAdapter(this,R.layout.activity_upcoming_events,events));
+        lv.setAdapter(new EventsAdapter(UpcomingEventsActivity.this,R.layout.upcoming_event_row,events));
 
 
     }
 
     private void initializeDummyEvents() {
 
-        UpcomingEventObject event1 = new UpcomingEventObject(1,"George",545983177,10,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event1 = new UpcomingEventObject(1,"George Aoun",545983177,12,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event2 = new UpcomingEventObject(1,"Ahed Istaiteh",545922731,10,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event3 = new UpcomingEventObject(1,"Einav Menkov",525994121,5,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event4 = new UpcomingEventObject(1,"Luna Zreik",505452311,25,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event5 = new UpcomingEventObject(1,"David Cohen",545123327,8,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event6 = new UpcomingEventObject(1,"Avi Harari",545922477,30,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event7 = new UpcomingEventObject(1,"Roey Levi",525544111,2,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event8 = new UpcomingEventObject(1,"Omer Avner",505343131,40,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event9 = new UpcomingEventObject(1,"Firas Odeh",525019921,16,"BlaBla", Calendar.getInstance().getTime());
+        UpcomingEventObject event10 = new UpcomingEventObject(1,"Charbel Aoun",545987161,2,"BlaBla", Calendar.getInstance().getTime());
+
         events.add(event1);
+        events.add(event2);
+        events.add(event3);
+        events.add(event4);
+        events.add(event5);
+        events.add(event6);
+        events.add(event7);
+        events.add(event8);
+        events.add(event9);
+        events.add(event10);
+
     }
+
 
     public class EventsAdapter extends ArrayAdapter<UpcomingEventObject> {
 
@@ -63,17 +86,23 @@ public class UpcomingEventsActivity extends AppCompatActivity {
 
             TextView contactName, contactNumber, numberOfPeople, dateTime;
 
-            contactName = (TextView)findViewById(R.id.contantname);
-            contactNumber = (TextView)findViewById(R.id.contantnumber);
-            numberOfPeople = (TextView)findViewById(R.id.numberofpeople);
-            dateTime = (TextView)findViewById(R.id.dateTime);
+            contactName = (TextView)customView.findViewById(R.id.contactname);
+            contactNumber = (TextView)customView.findViewById(R.id.contactnumber);
+            numberOfPeople = (TextView)customView.findViewById(R.id.numberofpeople);
+            dateTime = (TextView)customView.findViewById(R.id.dateTime);
 
             contactName.setText(modelsArrayList.get(position).getContactName());
-            contactNumber.setText(modelsArrayList.get(position).getPhoneNum());
-            numberOfPeople.setText(modelsArrayList.get(position).getNumOfPeople());
+            contactNumber.setText(Integer.toString(modelsArrayList.get(position).getPhoneNum()));
+            numberOfPeople.setText(Integer.toString(modelsArrayList.get(position).getNumOfPeople()));
             dateTime.setText(modelsArrayList.get(position).getTimeDate().toString());
 
             return customView;
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
